7f3404445109306b5b4fcd6e385458c7fe3719df
Vishnu <vprasad@umass.edu>
2017-12-04 12:39:40 -0500

    Added a search bar

--- demo/screens/SearchScreen.js
+++ demo/screens/SearchScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import { SearchBar } from 'react-native-elements';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -8,13 +9,8 @@ export default class LinksScreen extends React.Component {
   };
 
   render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
-    );
+    return (<SearchBar
+        placeholder='Hello World'/>);
   }
 }
 

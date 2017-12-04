3db445253d0fbf4485681334c99bf7567b5e4545
Oliver Pang <opang23@gmail.com>
2017-12-04 18:40:28 -0500

    Profile buttons
    Added profile buttons from ExpoLinkViews.js

--- demo/screens/ProfileScreen.js
+++ demo/screens/ProfileScreen.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { ScrollView, StyleSheet, SectionList, Image, Text, View  } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import { Constants } from 'expo';
+import { WebBrowser } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+import Touchable from 'react-native-platform-touchable';
 
 export default class ProfileScreen extends React.Component {
   static navigationOptions = {
@@ -15,7 +18,7 @@ export default class ProfileScreen extends React.Component {
       //   {/* Go ahead and delete ExpoLinksView and replace it with your
       //      * content, we just wanted to provide you with some helpful links */}
       //
-      //</ScrollView>
+      <View>
       <View style={styles.titleContainer}>
         <View style={styles.titleIconContainer}>
             <Image
@@ -39,7 +42,81 @@ export default class ProfileScreen extends React.Component {
           </Text>
         </View>
       </View>
+
+      <View>
+        <Touchable
+          style={styles.option}
+          background={Touchable.Ripple('#ccc', false)}
+          onPress={this._handlePressDocs}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.optionIconContainer}>
+              <Image
+                source={{ uri: 'https://s3.amazonaws.com/exp-brand-assets/ExponentEmptyManifest_192.png' }}
+                resizeMode="contain"
+                fadeDuration={0}
+                style={{ width: 20, height: 20, marginTop: 1 }}
+              />
+            </View>
+            <View style={styles.optionTextContainer}>
+              <Text style={styles.optionText}>
+                Read the Expo documentation
+              </Text>
+            </View>
+          </View>
+        </Touchable>
+
+        <Touchable
+          background={Touchable.Ripple('#ccc', false)}
+          style={styles.option}
+          onPress={this._handlePressSlack}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.optionIconContainer}>
+              <Image
+                source={{ uri: 'https://s3.amazonaws.com/exp-brand-assets/ExponentEmptyManifest_192.png' }}
+                fadeDuration={0}
+                style={{ width: 20, height: 20 }}
+              />
+            </View>
+            <View style={styles.optionTextContainer}>
+              <Text style={styles.optionText}>
+                Join us on Slack
+              </Text>
+            </View>
+          </View>
+        </Touchable>
+
+        <Touchable
+          style={styles.option}
+          background={Touchable.Ripple('#ccc', false)}
+          onPress={this._handlePressForums}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.optionIconContainer}>
+              <Ionicons name="ios-chatboxes" size={22} color="#ccc" />
+            </View>
+            <View style={styles.optionTextContainer}>
+              <Text style={styles.optionText}>
+                Ask a question on the Expo forums
+              </Text>
+            </View>
+          </View>
+        </Touchable>
+      </View>
+    </View>
+    //</ScrollView>
     );
+
+
+    _handlePressSlack = () => {
+      WebBrowser.openBrowserAsync('https://slack.expo.io');
+    };
+
+    _handlePressDocs = () => {
+      WebBrowser.openBrowserAsync('http://docs.expo.io');
+    };
+
+    _handlePressForums = () => {
+      WebBrowser.openBrowserAsync('http://forums.expo.io');
+    };
   }
 
 }
@@ -106,5 +183,25 @@ const styles = StyleSheet.create({
   },
   colorTextContainer: {
     flex: 1,
+  },
+  optionsTitleText: {
+    fontSize: 16,
+    marginLeft: 15,
+    marginTop: 9,
+    marginBottom: 12,
+  },
+  optionIconContainer: {
+    marginRight: 9,
+  },
+  option: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#EDEDED',
+  },
+  optionText: {
+    fontSize: 15,
+    marginTop: 1,
   },
 });

397418a73ae91e827faf73d89c24c9b462ba2cb0
Oliver Pang <opang23@gmail.com>
2017-12-04 18:11:11 -0500

    Profile page initial changes
    Added picture and name

--- demo/screens/ProfileScreen.js
+++ demo/screens/ProfileScreen.js
@@ -1,27 +1,110 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, SectionList, Image, Text, View  } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import { Constants } from 'expo';
 
 export default class ProfileScreen extends React.Component {
   static navigationOptions = {
     title: 'Profile',
   };
 
+
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      //<ScrollView style={styles.container}>
+      //   {/* Go ahead and delete ExpoLinksView and replace it with your
+      //      * content, we just wanted to provide you with some helpful links */}
+      //
+      //</ScrollView>
+      <View style={styles.titleContainer}>
+        <View style={styles.titleIconContainer}>
+            <Image
+              source={{ uri: 'https://s3.amazonaws.com/exp-brand-assets/ExponentEmptyManifest_192.png' }}
+              style={{ width: 64, height: 64 }}
+              resizeMode="cover"
+            />
+        </View>
+
+        <View style={styles.titleTextContainer}>
+          <Text style={styles.nameText} numberOfLines={1}>
+            {"Username"}
+          </Text>
+
+          <Text style={styles.slugText} numberOfLines={1}>
+            {"slug"}
+          </Text>
+
+          <Text style={styles.descriptionText}>
+            {"User desc"}
+          </Text>
+        </View>
+      </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  titleContainer: {
+    paddingHorizontal: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    flexDirection: 'row',
+  },
+  titleIconContainer: {
+    marginRight: 15,
+    paddingTop: 2,
+  },
+  sectionHeaderContainer: {
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ededed',
+  },
+  sectionHeaderText: {
+    fontSize: 14,
+  },
+  sectionContentContainer: {
+    paddingTop: 8,
+    paddingBottom: 12,
+    paddingHorizontal: 15,
+  },
+  sectionContentText: {
+    color: '#808080',
+    fontSize: 14,
+  },
+  nameText: {
+    fontWeight: '600',
+    fontSize: 18,
+  },
+  slugText: {
+    color: '#a39f9f',
+    fontSize: 14,
+    backgroundColor: 'transparent',
+  },
+  descriptionText: {
+    fontSize: 14,
+    marginTop: 6,
+    color: '#4d4d4d',
+  },
+  colorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  colorPreview: {
+    width: 17,
+    height: 17,
+    borderRadius: 2,
+    marginRight: 6,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ccc',
+  },
+  colorTextContainer: {
+    flex: 1,
   },
 });

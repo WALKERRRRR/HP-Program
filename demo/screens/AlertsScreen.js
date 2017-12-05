5ef717fb4435b54d68e5657ae75aa8567826a937
Chang Ding Yang <oscaryang12@yahoo.com>
2017-12-04 19:56:48 -0500

    Changed alert titles to be compatible with Android as well

--- demo/screens/AlertsScreen.js
+++ demo/screens/AlertsScreen.js
@@ -1,14 +1,83 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React, { Component } from 'react';
+import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
 
-export default class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Alerts',
-  };
-
+export default class SectionListBasics extends Component {
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View style={styles.container}>
+
+          <View style={styles.urgentTitle}>
+            <Text style={styles.titleText}>Urgent Alerts</Text>
+          </View>
+        <SectionList
+          sections={[
+            {data: ['A', 'B', 'C', 'D', 'E', 'F', 'B', 'C', 'D']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          keyExtractor={(item, index) => index}
+        />
+        <View style={styles.cautionTitle}>
+            <Text style={styles.titleText}>Caution Alerts</Text>
+        </View>
+        <SectionList
+          sections={[
+            {data: ['A', 'B', 'C', 'D', 'E', 'F', 'B', 'C', 'D', 'E', 'F', 'B', 'C', 'D', 'E', 'F']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          keyExtractor={(item, index) => index}
+        />
+        <View style={styles.neutralTitle}>
+          <Text style={styles.titleText}>Neutral Alerts</Text>
+        </View>
+        <SectionList
+          sections={[
+            { data: ['A', 'B', 'C', 'D', 'E', 'F', 'B', 'C', 'D', 'E', 'F', 'B', 'C', 'D', 'E', 'F']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex:1,
+   paddingTop: 22
+  },
+  urgentTitle: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    backgroundColor: 'red',
+  },
+  titleText:{
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  cautionTitle: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    backgroundColor: 'rgba(247,247,24,1.0)',
+  },
+  neutralTitle: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    backgroundColor: 'rgba(24,247,24,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
+
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => SectionListBasics);

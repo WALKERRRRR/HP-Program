c50dec1f55c32697eaeb6c5d3b68c8105a1bd01f
Vishnu <vprasad@umass.edu>
2017-12-04 20:44:51 -0500

    Search bar now takes input string as a parameter

--- demo/screens/SearchScreen.js
+++ demo/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import { SearchBar } from 'react-native-elements';
 
@@ -8,9 +8,26 @@ export default class LinksScreen extends React.Component {
     title: 'Search',
   };
 
+  state: {
+    toChange: String,
+  }
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      toChange: ' ',
+    };
+  }
+
   render() {
-    return (<SearchBar
-        placeholder='Hello World'/>);
+    return (
+        <View>
+        <SearchBar
+        onChangeText={(toChange) => this.setState({toChange})}
+        placeholder='Hello World'/>
+        <Text> {' ' + this.state.toChange+ ' '} </Text>
+        </View>
+        );
   }
 }
 

557b78c2b418079dee6269de101d8a595fc68c71
Chang Ding Yang <oscaryang12@yahoo.com>
2017-12-05 14:13:49 -0500

    Update to Alerts

--- demo/screens/AlertsScreen.js
+++ demo/screens/AlertsScreen.js
@@ -1,5 +1,14 @@
-import React, { Component } from 'react';
-import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { AppRegistry, SectionList, StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import Touchable from 'react-native-platform-touchable';
+import { Ionicons } from '@expo/vector-icons';
+
+//TODO Change the titles to be buttons and have it expand. Change the text box to onPress maybe?
+
+const dataUrgent = ['A', 'B']
+const dataCaution = ['A', 'B', 'C', 'D', 'E']
+const dataNeutral = ['A', 'B', 'C', 'D', 'E', 'F', 'B', 'C', 'D', 'E', 'F', 'B', 'C', 'D', 'E', 'F']
+const signUrgent = "+"
 
 export default class SectionListBasics extends Component {
   static navigationOptions = {
@@ -8,34 +17,56 @@ export default class SectionListBasics extends Component {
   render() {
     return (
       <View style={styles.container}>
-          <View style={styles.urgentTitle}>
-            <Text style={styles.titleText}>Urgent Alerts</Text>
+        <Touchable
+          style={styles.optionUrgent}
+          background={Touchable.Ripple('#ccc', false)}
+          onPress={this._handlePressCngPass}>
+          <View style={{ flexDirection: 'row' }}>
+          <View style={styles.optionIconContainer}>
+            <Ionicons name="ios-warning" size={25} color="#ff0000" />
           </View>
+            <Text style={styles.titleText}>Urgent Alerts ({dataUrgent.length})</Text>
+            <Text style={styles.signText}> ({signUrgent}) </Text>
+            </View>
+        </Touchable>
         <SectionList style={styles.sectionStyle}
           sections={[
-            {data: ['A']},
+            {data: dataUrgent},
           ]}
           renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
           keyExtractor={(item, index) => index}
         />
 
-        <View style={styles.cautionTitle}>
-            <Text style={styles.titleText}>Caution Alerts</Text>
-        </View>
+
+        <Touchable
+          style={styles.optionCaution}
+          background={Touchable.Ripple('#ccc', false)}
+          onPress={this._handlePressCngPass}>
+          <View style={{ flexDirection: 'row' }}>
+          <View style={styles.optionIconContainer}>
+            <Ionicons name="ios-warning" size={25} color="#EAEA14" />
+          </View>
+            <Text style={styles.titleText}>Caution Alerts ({dataCaution.length})</Text>
+            <Text style={styles.signText}> ({signUrgent}) </Text>
+          </View>
+        </Touchable>
         <SectionList style={styles.sectionStyle}
           sections={[
-            {data: ['A', 'B', 'C', 'D', 'E']},
+            {data: dataCaution},
           ]}
           renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
           keyExtractor={(item, index) => index}
         />
-      
-        <View style={styles.neutralTitle}>
-          <Text style={styles.titleText}>Neutral Alerts</Text>
-        </View>
+
+        <Touchable
+          style={styles.optionNeutral}
+          background={Touchable.Ripple('#ccc', false)}
+          onPress={this._handlePressCngPass}>
+          <Text style={styles.titleText}>Neutral Alerts ({dataNeutral.length})</Text>
+        </Touchable>
         <SectionList style={styles.sectionStyle}
           sections={[
-            { data: ['A', 'B', 'C', 'D', 'E', 'F', 'B', 'C', 'D', 'E', 'F', 'B', 'C', 'D', 'E', 'F']},
+            {data: dataNeutral},
           ]}
           renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
           keyExtractor={(item, index) => index}
@@ -52,36 +83,47 @@ const styles = StyleSheet.create({
   sectionStyle:{
     minHeight: 50,
   },
-  urgentTitle: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    backgroundColor: 'red',
-  },
   titleText:{
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: 'bold',
-  },
-  cautionTitle: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    backgroundColor: 'rgba(247,247,24,1.0)',
-  },
-  neutralTitle: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    backgroundColor: 'rgba(24,247,24,1.0)',
   },
   item: {
     padding: 10,
-    fontSize: 18,
+    fontSize: 15,
     height: 44,
   },
+  optionUrgent: {
+    backgroundColor: '#EDEDED',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    marginBottom: 10,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#EDEDED',
+  },
+  optionCaution: {
+    backgroundColor: '#EDEDED',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    marginBottom: 10,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#EDEDED',
+  },
+  optionNeutral: {
+      backgroundColor: '#EDEDED',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    marginBottom: 10,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#EDEDED',
+  },
+  optionIconContainer: {
+    marginRight: 9,
+  },
+  signText: {
+    paddingHorizontal: 175,
+    textAlign: 'right',
+    fontSize: 15
+  }
 })
 
 

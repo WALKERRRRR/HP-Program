e302f5834ddcf00d25b1392cdc06e4f658a339f1
Jack Kenney <kenney.jnk@gmail.com>
2017-12-06 11:11:40 -0500

    made a 250 row file

--- DataConstruction/data_construction.py
+++ DataConstruction/data_construction.py
@@ -4,11 +4,10 @@ def contruct():
     dic = open('systems-2017-09-21.csv', 'r')
     data = []
     dataHeaders = dic.readline().replace('"', '').replace('\n', '').split(',')
-    print len(dataHeaders)
     rowCount = 0 # current row
     colCount = 0 # current header (data type)
     for line in dic:
-        if rowCount == 500:
+        if rowCount == 250:
             break
         data.append({})
         entries = line.replace('"','').replace('\n','').split(',')
@@ -26,7 +25,7 @@ def contruct():
 
 if __name__ == '__main__':
     data = contruct()
-    outfile = open('constructed-data-500.js', 'w')
+    outfile = open('constructed-data-250.js', 'w')
     outfile.write('global.data = ')
     json.dump(data, outfile)
     outfile.close()

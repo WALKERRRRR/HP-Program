c1ada4a05f4bd0a942c363806aa6c4fc4093355f
Jack Kenney <kenney.jnk@gmail.com>
2017-12-06 14:06:24 -0500

    made demo data with 2 companies ~120 rows

--- DataConstruction/data_construction.py
+++ DataConstruction/data_construction.py
@@ -7,25 +7,25 @@ def contruct():
     rowCount = 0 # current row
     colCount = 0 # current header (data type)
     for line in dic:
-        if rowCount == 250:
-            break
-        data.append({})
-        entries = line.replace('"','').replace('\n','').split(',')
-        entryNum = 0
-        for entry in entries:
-            entryNum += 1
-            if entryNum <= len(dataHeaders):
-                data[rowCount][dataHeaders[colCount]] = entry
-            colCount += 1
-        rowCount += 1
-        colCount = 0
+        # if rowCount == 250:
+        #     break
+        if 'Lynx' in line or 'Zombie' in line:
+            data.append({})
+            entries = line.replace('"','').replace('\n','').split(',')
+            colCount = 0
+            for entry in entries:
+                if colCount <= len(dataHeaders):
+                    print rowCount, ' ', colCount
+                    data[rowCount][dataHeaders[colCount]] = entry
+                    colCount += 1
+            rowCount += 1
     return data
 
 
 
 if __name__ == '__main__':
     data = contruct()
-    outfile = open('constructed-data-250.js', 'w')
+    outfile = open('constructed-data-demo.js', 'w')
     outfile.write('global.data = ')
     json.dump(data, outfile)
     outfile.close()

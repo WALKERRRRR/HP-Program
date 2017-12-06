9d485c6012345e3a7bbfe84b46b6a440dc8ebae0
Vishnu <vprasad@umass.edu>
2017-12-06 13:22:47 -0500

    Some search updates

--- demo/screens/SearchScreen.js
+++ demo/screens/SearchScreen.js
@@ -10,39 +10,62 @@ export default class LinksScreen extends React.Component {
   };
 
   state: {
-    toChange: String,
+    query: String,
+    tableHead: [],
+    tableTitle: [],
+    tableData: [],
+    toSearch: [],
   }
   
   constructor(props) {
     super(props);
     this.state = {
-      toChange: ' ',
+      query: ' ',
+      tableHead: ['companyName','systemName','serialNumber','productFamily','model','osVersion','cpgCount','recommended.osVersion','location.region','location.country','installDate'],
+      tableTitle: ['System 1', 'System 2'],
+      tableData: [
+        ['1', '2', '3', '4','5', '6','7','8','9','0','10','11'],
+        ['a', 'b', 'c', 'd','e','f','g','i','j','k','l','m','n','o'],
+      ],
+      toSearch: [{ "capacity.byType.ssd.sizeTiB": "9.75625", "cpgCount": "45", "serialNumber": "1", "capacity.total.compactionRatio": "6.599999904632568", "nodes.cpuAvgMax": "1", "capacity.total.sizeTiB": "67.06875", "performance.summary.portInfo.totalServiceTimeMillis": "0.8935664623975753", "performance.summary.portInfo.readServiceTimeMillis": "1.4700697481632232", "recommended.osVersion": "", "disks.total.diskCountNormal": "192", "performance.portBandwidthData.total.dataRateKBPSAvg": "20597.5", "capacity.byType.nl.freeTiB": "18.219140624999998", "performance.summary.vvInfo.vvsByType.ssd.writeBandwidthMBPS": "", "disks.total.diskCount": "192", "performance.summary.vvInfo.vvsByType.ssd.readServiceTimeMillis": "", "capacity.total.freePct": "53.699851989746094", "installDate": "2014-03-12T16:45:27Z", "performance.portBandwidthData.total.iopsAvg": "978.0", "performance.summary.portInfo.writeServiceTimeMillis": "0.3076309263706207", "tpvvCount": "30", "location.region": "Americas", "performance.summary.vvInfo.vvsByType.ssd.writeServiceTimeMillis": "", "capacity.byType.ssd.freeTiB": "7.4976562499999995", "nodes.nodeCount": "6", "capacity.total.freeTiB": "36.0158203125", "updated": "2017-09-20T09:30:13Z", "capacity.byType.nl.sizeTiB": "39.4625", "vvCount": "36", "companyName": "Frenzy", "performance.summary.delAckPct": "", "disksState": "normal", "disks.total.diskCountFailed": "", "capacity.total.dedupeRatio       ": "", "osVersion": "3.2.2.612 (MU4)", "capacity.total.compressionRatio": "", "productFamily": "7000", "disks.total.diskCountDegraded": "", "capacity.byType.fc.sizeTiB": "17.849999999999998", "performance.portBandwidthData.total.iopsMax": "1454.2", "nodes.nodeCountOffline": "0", "systemName": "Ricadonna", "capacity.byType.fc.freeTiB": "10.299023437499999", "location.country": "US", "model": "7200", "performance.summary.vvInfo.vvsByType.ssd.readBandwidthMBPS": "" }, { "capacity.byType.ssd.sizeTiB": "", "cpgCount": "6", "serialNumber": "2", "capacity.total.compactionRatio": "4.300000190734863", "nodes.cpuAvgMax": "1", "capacity.total.sizeTiB": "26.873437499999998", "performance.summary.portInfo.totalServiceTimeMillis": "2.3", "performance.summary.portInfo.readServiceTimeMillis": "1.738500040769577", "recommended.osVersion": "3.2.2(EMU4)", "disks.total.diskCountNormal": "72", "performance.portBandwidthData.total.dataRateKBPSAvg": "98430.5", "capacity.byType.nl.freeTiB": "", "performance.summary.vvInfo.vvsByType.ssd.writeBandwidthMBPS": "", "disks.total.diskCount": "72", "performance.summary.vvInfo.vvsByType.ssd.readServiceTimeMillis": "", "capacity.total.freePct": "61.79792404174805", "installDate": "2016-07-25T17:07:37Z", "performance.portBandwidthData.total.iopsAvg": "546.0", "performance.summary.portInfo.writeServiceTimeMillis": "2.0", "tpvvCount": "18", "location.region": "Americas", "performance.summary.vvInfo.vvsByType.ssd.writeServiceTimeMillis": "", "capacity.byType.ssd.freeTiB": "", "nodes.nodeCount": "6", "capacity.total.freeTiB": "16.6072265625", "updated": "2017-09-20T09:22:16Z", "capacity.byType.nl.sizeTiB": "", "vvCount": "24", "companyName": "Shanga", "performance.summary.delAckPct": "", "disksState": "normal", "disks.total.diskCountFailed": "", "capacity.total.dedupeRatio       ": "", "osVersion": "3.2.2.530 (MU3)", "capacity.total.compressionRatio": "", "productFamily": "8000", "disks.total.diskCountDegraded": "", "capacity.byType.fc.sizeTiB": "26.873437499999998", "performance.portBandwidthData.total.iopsMax": "679.8000000000001", "nodes.nodeCountOffline": "0", "systemName": "Brainchild", "capacity.byType.fc.freeTiB": "16.6072265625", "location.country": "MX", "model": "8200", "performance.summary.vvInfo.vvsByType.ssd.readBandwidthMBPS": "" }],
     };
   }
 
-  render() {
-    const tableHead = ['companyName','systemName','serialNumber','productFamily','model','osVersion','cpgCount','recommended.osVersion','location.region','location.country','installDate'];
-    const tableTitle = ['System 1','System 2'];
-    const tableData = [
-      ['1', '2', '3', '4','5', '6','7','8','9','0','10','11'],
-      ['a', 'b', 'c', 'd','e','f','g','i','j','k','l','m','n','o'],
+  handleSearch = (text) => {
+    this.setState({ query: text });
+    tT = [];
+    tD = [];
+    tS = this.state.toSearch;
+    tT = ["One System"];
+    tD = [
+      ['1', '2', '3', '4', '5', '6','7','8','9','0','10','11'],
+      ['2', '4', '4', '2', '6', '6','4','6','2','8','3','45'],
     ];
-        const widthArr = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100];
+    filtered = tS.filter(sys => sys.serialNumber > 0);
+    tT = filtered.map((sys) => sys.cpgCount);
+    this.setState({ tableTitle: tT});
+    this.setState({ tableData: tD});
+  }
+
+  
+
+  render() {
+        widthArr = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100];
 
     return (
         <View>
         <SearchBar
-        onChangeText={(toChange) => this.setState({toChange})}
+        onChangeText = {this.handleSearch}
         placeholder='Search'/>
-        <Text> {' ' + this.state.toChange+ ' '} </Text>
+        <Text> {' ' + this.state.query+ ' '} </Text>
 
 
         <Table style={styles.table}>
           {/* Left Wrapper */}
           <TableWrapper style={{width: 80}}>
-            <Cell data="Head" style={styles.head} textStyle={styles.headText}/>
+            <Cell data= {''+ this.state.query+ ''} style={styles.head} textStyle={styles.headText}/>
             {
-              tableTitle.map((title, i) => (
+              this.state.tableTitle.map((title, i) => (
                 <Cell key={i} data={title} height={28} style={i%2 && {backgroundColor: '#DFF5F2'}} textStyle={styles.titleText}/>
               ))
             }
@@ -52,9 +75,9 @@ export default class LinksScreen extends React.Component {
           <ScrollView horizontal={true}>
             {/* If parent element is not table element, you should add the type of borderstyle. */}
             <TableWrapper borderStyle={{borderWidth: 1,borderColor: '#000',}}>
-              <Row data={tableHead} style={styles.head} textStyle={styles.headText} widthArr={widthArr}/>
+              <Row data={this.state.tableHead} style={styles.head} textStyle={styles.headText} widthArr={widthArr}/>
               {
-                tableData.map((data, i) => (
+                this.state.tableData.map((data, i) => (
                   <Row key={i} data={data} style={[styles.list, i%2 && {backgroundColor: '#DFF5F2'}]} widthArr={widthArr} textStyle={styles.listText}/>
                 ))
               }

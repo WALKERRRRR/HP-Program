8cb8ce0769d2432c8f2c08412a26b529d120eb91
Jack Kenney <kenney.jnk@gmail.com>
2017-12-05 22:29:30 -0500

    constructed the data!

--- DataConstruction/data_construction.py
+++ DataConstruction/data_construction.py
@@ -1,20 +1,28 @@
+import json
 
-def construct(data):
-
-    return
-
-
-def getData():
+def contruct():
     dic = open('systems-2017-09-21.csv', 'r')
-    data = []
-    dataHeaders = dic.readline
+    data = [{}]
+    dataHeaders = dic.readline().replace('"', '').replace('\n', '').split(',')
+    print len(dataHeaders)
+    rowCount = 0 # current row
+    colCount = 0 # current header (data type)
     for line in dic:
-        return
+        data.append({})
+        entries = line.replace('"','').replace('\n','').split(',')
+        entryNum = 0
+        for entry in entries:
+            entryNum += 1
+            if entryNum <= len(dataHeaders):
+                data[rowCount][dataHeaders[colCount]] = entry
+            colCount += 1
+        rowCount += 1
+        colCount = 0
+    return data
 
 
 
 if __name__ == '__main__':
-    data = getData()
-    data = construct(data)
-    f = open('constructed-data', 'w')
-    f.write(data)
+    data = contruct()
+    with open('constructed-data.txt', 'w') as outfile:
+        json.dump(data, outfile)
